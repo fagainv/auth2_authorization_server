97befc73f4df523f6527945fb1b0b4e4c9da2367
vfranzoni <vfranzoni@onemagnify.com>
2024-11-07 10:03:16 -0500

    get the username if it comes

---
+++ application/src/main/java/com/om/auth2/as/controller/LoginController.java
@@ -0,0 +1,20 @@
+package com.om.auth2.as.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+
+@Controller
+public class LoginController {
+
+	
+	@GetMapping("/login")
+	public String getLoginPage(@RequestParam(value = "username", required = false) String username, Model model) {
+		
+		model.addAttribute("username", username);
+		return "login";
+		
+	}
+}
